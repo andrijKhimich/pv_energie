80d3add87f1fec0b055952ae5f91f6b33f965d46
Andrew Khimich <andrewkhimich@Andrews-MacBook-Air.local>
2025-02-02 21:02:07 +0100

    base home page

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,28 @@
-// import {sum} from './modules/sum.js';
 // import libs from './modules/libs.js';
 // import $ from 'jquery';
+// import Swiper from "swiper";
 
-import {toggleTab} from './modules/tabs.js';
+import {toggleTab} from "./modules/tabs.js";
 toggleTab();
 
-import {toggleMenu} from './modules/toggleMenu.js';
+import {toggleMenu} from "./modules/toggleMenu.js";
 toggleMenu();
 
-import {toggleAccordion} from './modules/toggleAccordion.js';
+import {toggleAccordion} from "./modules/toggleAccordion.js";
 toggleAccordion();
 
-import {showMoreText} from './modules/showMoreText.js';
+import {showMoreText} from "./modules/showMoreText.js";
 showMoreText();
+
+var swiper = new Swiper(".hero-slider", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
